1680989688b3eb9e432da1988d48f63ddd87a120
richskills <richskills@users.noreply.github.com>
2021-02-17 15:04:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -88,17 +88,19 @@
             <img src="assets/img/about.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
-            <h3>Voluptatem dignissimos provident quasi corporis</h3>
+            <h3>We've got what you need!</h3>
             <p class="font-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+            Vametech is an organization created to carter for the needs of technology industry advancement in this century
+            based in Dar es salaam Tanzania dealing whith development of different solutions on different kind of problems 
+            in all human aspects in terms of software and hardware in any kind of platform., and
+            computer Programs
             </p>
             <ul>
-              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="icofont-check-circled"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              <li><i class="icofont-check-circled"></i> Mobile App.</li>
+              <li><i class="icofont-check-circled"></i> Web services.</li>
+              <li><i class="icofont-check-circled"></i> computer Programs.</li>
             </ul>
-            <a href="#" class="read-more">Read More <i class="icofont-long-arrow-right"></i></a>
+           
           </div>
         </div>
 
